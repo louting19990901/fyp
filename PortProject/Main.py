759ec40864f59c7179584fc2ab56c67e139bb5f8
louting19990901 <463166226@qq.com>
2022-04-24 15:03:51 +0800

    clear

--- PortProject/Main.py
+++ PortProject/Main.py
@@ -1,6 +1,8 @@
 import gym
-# from py_client_sb import YardEnv
-from py_client_v2 import YardEnv
+# from py_client_v1 import YardEnv
+# from py_client_v2 import YardEnv
+# from py_client_v3 import YardEnv
+from py_client_v4 import YardEnv
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 import time
@@ -22,23 +24,6 @@ import os
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
 global_relocation_list=[]
-test_relocation_list=[]
-
-def make_env(env_id, rank, seed=0):
-    """
-    Utility function for multiprocessed env.
-
-    :param env_id: (str) the environment ID
-    :param num_env: (int) the number of environments you wish to have in subprocesses
-    :param seed: (int) the inital seed for RNG
-    :param rank: (int) index of the subprocess
-    """
-    def _init():
-        env = YardEnv(16,env_id,"train",global_relocation_list)
-        env.seed(seed + rank)
-        return env
-    set_random_seed(seed)
-    return _init
 
 def getMean(arr):
     sum=0.0
@@ -47,12 +32,8 @@ def getMean(arr):
     return float(sum)/float(len(arr))
 
 if __name__ == '__main__':
-    # env_id = "CartPole-v1"
-    num_cpu = 4  # Number of processes to use
     episode = 2000
     total_task_number = 200
-
-    # envs=[make_env(i + 20, i + 20) for i in range(num_cpu)]
 
     env=YardEnv(16,29,'train',global_relocation_list)
     # env = SubprocVecEnv(envs)
@@ -63,20 +44,10 @@ if __name__ == '__main__':
     model.learn(total_timesteps=total_task_number*episode)
     toc = time.time()
     due = toc - tic
-    print(num_cpu," core take ",due)
-
-
-    # print(global_relocation_list)
-    # eval_env = YardEnv(16, 50, 'test',test_relocation_list)
-    # mean_reward, std_reward = evaluate_policy(model, eval_env, n_eval_episodes=1)
-    # print(f"mean_reward:{mean_reward:.2f} +/- {std_reward:.2f}")
-
-
 
     print("total mean ",getMean(global_relocation_list))
-    print("best min index is ",env.bestMeanIndex)
-    print("best min is ",env.bestMean)
-    # print("last 100 mean ",getMean(global_relocation_list[-100:]))
+    print("The episode of Min Average NAR is ",env.bestMeanIndex)
+    print("Min Average NAR is ",env.bestMean)
     plt.plot( range(len(global_relocation_list)),global_relocation_list)
     plt.show()
 
--- PortProject/py_client_v4.py
+++ PortProject/py_client_v4.py
@@ -10,7 +10,6 @@ import torch.nn as nn
 import gym
 import torch.nn.functional as F
 import math
-
 import gym
 from gym import spaces
 from stable_baselines3.common.env_checker import check_env
@@ -41,11 +40,13 @@ class Observation:  # total 174 bit => total 30
         self.queuingContainers = queuingContainers
         self.relocationNumber = relocationNumber
 
+
 def getMean(arr):
-    sum=0.0
+    sum = 0.0
     for i in arr:
-        sum+=i
-    return float(sum)/float(len(arr))
+        sum += i
+    return float(sum) / float(len(arr))
+
 
 class YardEnv(gym.Env):
     """Custom Environment that follows gym interface"""
@@ -58,15 +59,14 @@ class YardEnv(gym.Env):
     episode_list = []
     last100relocationNumbers = np.zeros(100)
     last100Rewards = np.zeros(100)
-    global_relocation_list=[]
-    additionalRelocation=0
-    bestMean=99999
-    bestMeanIndex=-1
+    global_relocation_list = []
+    additionalRelocation = 0
+    bestMean = 99999
+    bestMeanIndex = -1
 
-    def __init__(self, n_actions, port, env_type,global_relocation_list):
+    def __init__(self, n_actions, port, env_type, global_relocation_list):
         super(YardEnv, self).__init__()
         self.client = None
-        # print("ini port is ",port)
         self.port = port
         self.is_connected = False
 
@@ -74,56 +74,28 @@ class YardEnv(gym.Env):
         self.env_type = env_type
         self.root_path = os.getcwd()
         self.executor = self.start_java_end()
-        self.global_relocation_list=global_relocation_list
+        self.global_relocation_list = global_relocation_list
         self.action_space = spaces.Discrete(6)
-        # Example for using image as input (channel-first; channel-last also works):
-
         self.observation_space = spaces.Box(low=0, high=25,
                                             shape=(30,), dtype=np.uint8)
 
         # start server and client
 
     def start_java_end(self):
-        # print("self.port: ",self.port)
-        # print("1: ",os.getcwd(),type(os.getcwd()))
-
-        # if (os.getcwd() != r"C:\Users\86189\Desktop\fyp\fyp\PortProject"):
-        #     os.chdir("..")
-        # os.chdir("..")
-        # print("2: ", os.getcwd())
+        # locate the directory
         os.chdir("C:/Users/86189/Desktop/fyp/fyp/PortProject/JavaProject/bin")
 
-        # print("3: ", os.getcwd())
-
+        # call Java file
         jvmPath = jpype.getDefaultJVMPath()
         jar_path = '-Djava.class.path={}'.format(
             self.get_jars(self.root_path))
         if not jpype.isJVMStarted():
             jpype.startJVM(jvmPath, jar_path)
-        # print("4:",os.getcwd())
         javaClass = jpype.JClass("Environment.Executor")
 
-        '''
-        # constructor
-        #         javaInstance = javaClass(1,2)
-        # 
-        #         # att ributes
-        #         print(javaInstance.a)
-        #         print(javaInstance.b)	
-        # 
-        #         # invoke function
-        result = javaInstance.test_func(123)
-        print(result)
-        '''
-        # print("5: ", os.getcwd())
         os.chdir("..")
-        # print("6: ", os.getcwd())
-        # print("current port: ",self.port+self.zero_port)
         executor = javaClass(self.zero_port + self.port, self.env_type)  # get an instance of java object
 
-        # javaInstance = javaClass.getInstance('demo')  # invoke class static function directly
-
-        # jpype.shutdownJVM()
         '''
         OSError: JVM cannot be restarted
         jpype.startJVM(jvmPath)
@@ -151,7 +123,6 @@ class YardEnv(gym.Env):
         self.executor.startServer()
 
         # connect to server
-        # print("port ",self.port," connect: ",self.is_connected)
         if not self.is_connected:
             self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.client.connect(('127.0.0.1', self.zero_port + self.port))
@@ -159,7 +130,7 @@ class YardEnv(gym.Env):
 
         info = json.loads(str(self.client.recv(1024), encoding="GBK"))
 
-        # -----------state-----------
+        # extract information from json format
         bay = info.get("bay")
         stack = info.get("stack")
         containersMatrix = info.get("containersMatrix")
@@ -169,14 +140,13 @@ class YardEnv(gym.Env):
         queuingContainers = info.get("queuingContainers")
         taskNumber = info.get("taskNumber")
         relocationNumber = info.get("relocationNumber")
-        # reward = info.get('reward')
 
-        containerRelocationNumber=info.get("containerRelocationNumber")
+        containerRelocationNumber = info.get("containerRelocationNumber")
         is_done = info.get('isDone')
-        # print("containerRelocationNumber",containerRelocationNumber)
 
+        # show information received
         if show_info:
-            print("reset****")
+            # print("reset****")
             print("bay: ", type(bay), " : ", bay)
             print("stack: ", type(stack), " : ", stack)
             print("containerMatrix: ", type(containersMatrix), " : ", containersMatrix)
@@ -189,16 +159,19 @@ class YardEnv(gym.Env):
             print("is_done: ", type(is_done), " : ", is_done)
         headingContainers = self.regulateStrings(headingContainers)
         queuingContainers = self.regulateStrings(queuingContainers)
-        obs = Observation(bay, stack, containersMatrix, headingTrucksNumber, queuingTrucksNumber, headingContainers,
+        # copy the observation in the environment so that it can be used in next step
+        obs = Observation(bay, stack, containersMatrix, headingTrucksNumber,queuingTrucksNumber, headingContainers,
                           queuingContainers, relocationNumber)
         self.observation = obs
         s = self.getState(obs)
         return s
 
+    # string formatting
     def receive_end_info(self):
         self.client.send(str(-1).encode('GBK'))  # send episode final info request
         end_info = json.loads(str(self.client.recv(1024 * 10), encoding="GBK"))
 
+    # transfer information to specific format
     def regulateStrings(self, str):
         if str == None or len(str) == 0:
             str = "0000000000"
@@ -208,6 +181,7 @@ class YardEnv(gym.Env):
             str = str[0:10]
         return str
 
+    # collect and combine all the information to form one single string which represent the status)
     def getState(self, obs):
         # bay   1
         # stack   1
@@ -233,45 +207,33 @@ class YardEnv(gym.Env):
         s = np.array(s).astype(np.uint8)
         return s
 
-    def checkAction(self,action:int):
+    # check the validity of the action in certain status and change the action into valid one
+    def checkAction(self, action: int):
 
         while (True):
             pileSize = int(self.observation.containersMatrix[self.observation.bay * 6 + action])
-
-
-            if (action != self.observation.stack) and (pileSize <6):
+            if (action != self.observation.stack) and (pileSize < 6):
                 return action
             else:
-                # action = np.random.randint(0, 6)
+                action = (action + 1) % 6
 
-                action=(action+1)%6
-                # print("change to ", action)
-                # print("this should not prompt")
-
-    def checkActionValid(self,action:int):
+    # check the validity of the action in certain status
+    def checkActionValid(self, action: int):
         pileSize = int(self.observation.containersMatrix[self.observation.bay * 6 + action])
-        if (action != self.observation.stack) and (pileSize <6):
+        if (action != self.observation.stack) and (pileSize < 6):
             return True
         else:
             return False
 
-
-
-    def calculateReward(self,action,bay):
-        r=0
-
-
+    # calculate the reward received in current step based on the chosen action
+    def calculateReward(self, action, bay):
+        r = 0
         if action == self.observation.stack:
-            # print("not move")
-            r-=5
-
-
+            r -= 5
 
         # check the layer of new position
-        # reward+=4-layer
         pileSize = int(self.observation.containersMatrix[self.observation.bay * 6 + action])
-        r = 0.5-1.0/6.0*pileSize
-        # print("pile$",r)
+        r = 0.5 - 1.0 / 6.0 * pileSize
 
         # check whether the new position will be the reshuffled in near future
         # if so, reward-=10
@@ -281,38 +243,40 @@ class YardEnv(gym.Env):
         #
         # print("heading bay",self.observation.headingContainers[1:3]," ",self.observation.headingContainers[6:8],", bay ",bay)
         # print("head stack",self.observation.headingContainers[3]," ",self.observation.headingContainers[8],", action ",action)
-        if(int(self.observation.queuingContainers[1:3])==bay and self.observation.queuingContainers[3]==action) or (int(self.observation.queuingContainers[6:8])==bay and self.observation.queuingContainers[8]==action):
-            r-=3
+        if (int(self.observation.queuingContainers[1:3]) == bay and self.observation.queuingContainers[
+            3] == action) or (
+                int(self.observation.queuingContainers[6:8]) == bay and self.observation.queuingContainers[
+            8] == action):
+            r -= 3
             # print("queue $")
 
-
-        if (int(self.observation.headingContainers[1:3]) == bay and self.observation.headingContainers[3] == action) or (
-                int(self.observation.headingContainers[1:3]) == bay and self.observation.headingContainers[8] == action):
+        # calculate reward regarding future containers
+        if (int(self.observation.headingContainers[1:3]) == bay and self.observation.headingContainers[
+            3] == action) or (
+                int(self.observation.headingContainers[1:3]) == bay and self.observation.headingContainers[
+            8] == action):
             r -= 4
-            # print("head $")
 
-        # print("current $",r)
         return r
 
+    # after receiving new action, send the action to the simulation
+    # and receive the new state from the simulation
+    # calculate the reward
+    # return status, reward
+    # process the isDone signal
     def step(self, action: int):
-        reward=0
-        # print("============================")
+        reward = 0
         # check whether the the action is valid:
         # layer should less than 4
         # new position should different from th eold one
         if not self.checkActionValid(action):
-            # print("!!!!!!!!!")
             if (self.env_type == "test"):
-                # action = self.checkAction(action)
                 action = (action + 1) % 6
             else:
                 return self.getState(self.observation), -1000, False, {"validAction": False}
 
-        # print("           1111111")
         self.client.send(str(action).encode('GBK'))
-        # print("        22222222")
         info = json.loads(str(self.client.recv(1024), encoding="GBK"))
-        # print("         333333")
         bay = info.get("bay")
         stack = info.get("stack")
         containersMatrix = info.get("containersMatrix")
@@ -322,12 +286,10 @@ class YardEnv(gym.Env):
         queuingContainers = info.get("queuingContainers")
         taskNumber = info.get("taskNumber")
         relocationNumber = info.get("relocationNumber")
-        containerRelocationNumber=info.get("containerRelocationNumber")
-
+        containerRelocationNumber = info.get("containerRelocationNumber")
         # reward = info.get('reward')
         is_done = info.get('isDone')
-        reward += self.calculateReward(action,bay)
-
+        reward += self.calculateReward(action, bay)
 
         if not is_done:
             headingContainers = self.regulateStrings(headingContainers)
@@ -345,36 +307,26 @@ class YardEnv(gym.Env):
                 print(" relocationNumber/taskNumber: ", float(relocationNumber) / float(taskNumber))
                 if (containerRelocationNumber > 0):
                     print("containerRelocationNumber", containerRelocationNumber)
-                print("is_done: ", type(is_done), " : ", is_done)
-            # reward -= ( float(relocationNumber) / float(taskNumber) + 0.07) * 100
-            if(containerRelocationNumber>0):
-                reward-=1
-                self.additionalRelocation+=1
-            # print("reward ",reward)
+            if (containerRelocationNumber > 0):
+                reward -= 1
+                self.additionalRelocation += 1
             obs = Observation(bay, stack, containersMatrix, headingTrucksNumber, queuingTrucksNumber, headingContainers,
                               queuingContainers, relocationNumber)
-
             self.observation = obs
             s_ = self.getState(obs)
-
             return s_, reward, is_done, {"validAction": True}
         else:
-            # print("port ",self.port,": episode ",self.count," end, additionalRelocation: ",self.observation.relocationNumber)
-            # self.count+=1
-            # self.global_relocation_list.append(self.observation.relocationNumber)
-
-            print("port ",self.port,": episode ",self.count," end, additionalRelocation: ",self.additionalRelocation)
-            self.count+=1
+            # if it is the last step in current episode, store the results for every episode
+            print("port ", self.port, ": episode ", self.count, " end, additionalRelocation: ",
+                  self.additionalRelocation)
+            self.count += 1
             self.global_relocation_list.append(self.additionalRelocation)
-            self.additionalRelocation=0
-            cmean=getMean(self.global_relocation_list[-100:])
-
-            if(self.count>=100 and cmean<self.bestMean):
-                self.bestMean=cmean
-                self.bestMeanIndex=self.count
-
-            # print(self.global_relocation_list)
-            if(self.env_type=="test"):
+            self.additionalRelocation = 0
+            cmean = getMean(self.global_relocation_list[-100:])
+            if (self.count >= 100 and cmean < self.bestMean):
+                self.bestMean = cmean
+                self.bestMeanIndex = self.count
+            if (self.env_type == "test"):
                 self.relocation_list.append(self.observation.relocationNumber)
             return np.zeros(30).astype(np.uint8), -1, is_done, {"validAction": True}
 
@@ -383,36 +335,3 @@ class YardEnv(gym.Env):
 
     def close(self):
         pass
-
-    # def receive_end_info(self):
-    #     self.client.send(str(-1).encode('GBK'))  # send episode final info request
-    #     end_info = json.loads(str(self.client.recv(1024*10), encoding="GBK"))
-
-# env = YardEnv(16, 10010, 'train')
-# # If the environment don't follow the interface, an error will be thrown
-#
-# check_env(env, warn=True)
-#
-#
-# # wrap it
-# # env = make_vec_env(lambda: env, n_envs=1)
-# # Train the agent
-# model = A2C('MlpPolicy', env, verbose=1).learn(1000)
-# # model.learn(1000)
-
-
-# Test the trained agent
-# obs = env.reset()
-# n_steps = 20
-# for step in range(n_steps):
-#   action, _ = model.predict(obs, deterministic=True)
-#   print("Step {}".format(step + 1))
-#   print("Action: ", action)
-#   obs, reward, done, info = env.step(action)
-#   print('obs=', obs, 'reward=', reward, 'done=', done)
-#   env.render(mode='console')
-#   if done:
-#     # Note that the VecEnv resets automatically
-#     # when a done signal is encountered
-#     print("Goal reached!", "reward=", reward)
-#     break
